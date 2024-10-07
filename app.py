715f6d9298fbd01a3311fb05ba460ee41b297f6d
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2024-10-08 00:38:58 +1000

    still trying to deploy this using CDK

---
+++ app.py
@@ -0,0 +1,15 @@
+# /usr/bin/env python3
+"""
+boilerplate to make CDK work
+"""
+import aws_cdk as cdk
+from referee_lights_cdk.referee_lights_cdk_stack import RefereeLightsCdkStack
+
+app = cdk.App()
+RefereeLightsCdkStack(
+	app,
+	"RefereeLightsCdkStack",
+	env=cdk.Environment(account="001499655372", region="ap-southeast-2")
+)
+
+app.synth()
