940f28d4ba6cf00d63d8b4e6a254fbc5f594b9c0
Michael Kingston <71872141+mkingopng@users.noreply.github.com>
2025-03-31 20:13:36 +1000

    cleaning up so this stupid thing will deploy

--- app.py
+++ app.py
@@ -4,8 +4,19 @@ boilerplate to make CDK work
 """
 import aws_cdk as cdk
 from referee_lights_cdk.referee_lights_cdk_stack import RefereeLightsCdkStack
+from billing_alarm_stack import BillingAlarmStack
+# from monitoring_stack import MonitoringStack
 
 app = cdk.App()
+
+# Deploy the billing alarm stack to us-east-1
+# BillingAlarmStack(
+# 	app,
+# 	"BillingAlarmStack",
+# 	env=cdk.Environment(region="us-east-1"),
+# )
+
+# Deploy your main application to ap-southeast-2
 RefereeLightsCdkStack(
 	app,
 	"RefereeLightsCdkStack",
